c4f8fb4e25c1ed28b5ab7db8bd32c0f271c3dd1d
Danny2706 <danikume27@gmail.com>
2026-08-11 18:11:57 +0300

    res user

--- custom-addons/real_estate_sales_person/models/res_users.py
+++ custom-addons/real_estate_sales_person/models/res_users.py
@@ -1,7 +1,7 @@
-from odoo import models, fields, api
+# from odoo import models, fields, api
 
 
-class Users(models.Model):
-    _inherit = "res.users"
+# class Users(models.Model):
+#     _inherit = "res.users"
 
-    property_ids = fields.One2many("estate.property", "sales_id", string="Properties")
+#     property_ids = fields.One2many("estate.property", "sales_id", string="Properties")
